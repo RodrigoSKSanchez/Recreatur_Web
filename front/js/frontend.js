ac2e6491d4750efd6a404bdb3d9e4d2841bb1f5d
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2024-11-11 11:43:44 -0300

    adicionando depoimentos a partir do backend

--- front/js/frontend.js
+++ front/js/frontend.js
@@ -55,7 +55,11 @@ async function fazerLogin() {
 </div> */}
 function exibirDepoimentos(depoimentos) {
     const corpoDepoimentos = document.getElementById("depoimentos");
+    while (corpoDepoimentos.firstChild) {
+        corpoDepoimentos.removeChild(corpoDepoimentos.lastChild);
+    }
     for(let depoimento of depoimentos){
+        console.log(depoimento);
         const divDepoimento = document.createElement("div");
         divDepoimento.className = "col-lg-3 col-md-4 p-4 border border-secondary-subtle rounded-4 align-items-center bg-white";
         const textoDepoimento = document.createElement("h5");
@@ -74,4 +78,41 @@ async function obterDepoimentos() {
     const URLcompleta = `${protocolo}${baseURL}${depoimentosEndPoint}`;
     const depoimentos = (await axios.get(URLcompleta)).data;
     exibirDepoimentos(depoimentos);
+}
+
+async function adicionarDepoimento() {
+    const depoimentosEndPoint = '/depoimentos';
+    const URLcompleta = `${protocolo}${baseURL}${depoimentosEndPoint}`;
+
+    let textoDepoimentoInput = document.querySelector("#textoDepoimentoInput");
+    let infoPessoaInput = document.querySelector("#infoPessoaInput");
+    let textoDepoimento = textoDepoimentoInput.value;
+    let infoPessoa = infoPessoaInput.value;
+
+    if(textoDepoimento && infoPessoa){
+        try{
+            const depoimentos = (await axios.post (URLcompleta, {
+                texto_depoimento : textoDepoimento,
+                info_pessoa : infoPessoa})).data;
+            exibirDepoimentos(depoimentos);
+            exibirAlerta('.alert-modal-depoimentos', "Depoimento cadastrado com sucesso",
+                ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+        }
+        catch(e) {
+            exibirAlerta('.alert-modal-depoimentos', "Erro ao criar o depoimento", ['show',
+                'alert-danger'], ['d-none', 'alert-success'], 2000)
+        }
+    }
+    else{  
+        exibirAlerta('.alert-modal-depoimentos', 'Preencha todos os campos', ['show',
+            'alert-danger'], ['d-none', 'alert-success'], 2000);
+    }
+}
+
+async function editarDepoimento() {
+
+}
+
+async function removerDepoimento() {
+
 }
