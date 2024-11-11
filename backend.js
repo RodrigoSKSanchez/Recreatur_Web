350717f319434f46fc1b4d4f6e9f4e07c5f4a27f
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2024-11-10 21:04:35 -0300

    obtendo depoimentos a partir do backend

---
+++ backend.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+function base64_encode(imagem) {
+    let base64 = fs.readFileSync(imagem, {
+        encoding : "base64"
+    });
+    return base64;
+};
+
+const TemasAtuais = mongoose.model("Temas_Atuais", mongoose.Schema({
+    titulo : {type : String},
+    texto : {type : String},
+    imagem : {type : String} // A imagem é armazenada como uma string (base64)
+}));
+
+const Depoimento = mongoose.model("Depoimentos", mongoose.Schema({
+    texto_depoimento : {type : String},
+    info_pessoa : {type : String}
+}))
+
+const usuarioSchema = mongoose.Schema({
+    login: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+usuarioSchema.plugin(uniqueValidator);
+
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+
+async function conectarAoMongo() {
+    await mongoose.connect(process.env.DATABASE_URL);
+}
+
+app.post("/login", async (req, res) => {
+    const login = req.body.login;
+    const password = req.body.password;
+    const usuarioExiste = await Usuario.findOne({ login: login });
+    if (!usuarioExiste) {
+        return res.status(401).json({ mensagem: "login inválido" }).end();
+    }
+
+    // verificar senha e dar continuidade
+    const senhaValida = await bcrypt.compare(password, usuarioExiste.password);
+    if (!senhaValida) {
+        return res.status(401).json({ mensagem: "senha inválida" });
+    }
+    const token = jwt.sign(
+        { login: login },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+    res.status(200).json({ token: token });
+});
+
+app.get("/depoimentos", async (req, res) => {
+    const depoimentos = await Depoimento.find();
+    res.json(depoimentos);
+});
+
+app.post("/depoimentos", async (req, res) => {
+    const textoDepoimento = req.body.texto_depoimento;
+    const infoPessoa = req.body.info_pessoa;
+
+    const depoimento = new Depoimento({
+        texto_depoimento : textoDepoimento,
+        info_pessoa : infoPessoa
+    });
+    await depoimento.save();
+    const depoimentos = await Depoimento.find();
+    res.json(depoimentos);
+});
+
+app.listen(3000, () => {
+    try {
+        conectarAoMongo();
+        console.log("server up & running & connection ok");
+        console.log(process.env.JWT_SECRET)
+    }
+    catch (e) {
+        console.log("erro de conexão", e);
+    }
+});
---
+++ front/js/frontend.js
@@ -0,0 +1,77 @@
+const protocolo = 'http://';
+const baseURL = 'localhost:3000';
+
+function exibirAlerta(seletor, innerHTML, classesToAdd, classesToRemove,
+    timeout) {
+    let alert = document.querySelector(seletor)
+    alert.innerHTML = innerHTML
+    alert.classList.add(...classesToAdd)
+    alert.classList.remove(...classesToRemove)
+    setTimeout(() => {
+        alert.classList.remove('show')
+        alert.classList.add('d-none')
+    }, timeout)
+}
+
+async function fazerLogin() {
+    let usuarioLoginInput = document.querySelector('#usuarioLoginInput')
+    let passwordLoginInput = document.querySelector('#passwordLoginInput')
+    let usuarioLogin = usuarioLoginInput.value
+    let passwordLogin = passwordLoginInput.value
+    if (usuarioLogin && passwordLogin) {
+        try {
+            const loginEndpoint = '/login'
+            const URLCompleta = `${protocolo}${baseURL}${loginEndpoint}`
+            const response = await axios.post(
+                URLCompleta,
+                { login: usuarioLogin, password: passwordLogin }
+            )
+            console.log(response.data)
+            usuarioLoginInput.value = ""
+            passwordLoginInput.value = ""
+            exibirAlerta('.alert-modal-login', "Login efetuado com sucesso!",
+                ['show', 'alert-success'], ['d-none', 'alert-danger'], 2000)
+            const cadastrarFilmeButton =
+                document.querySelector('#cadastrarFilmeButton')
+            cadastrarFilmeButton.disabled = false
+        }
+        catch (error) {
+            exibirAlerta('.alert-modal-login', "Erro ao fazer login", ['show',
+                'alert-danger'], ['d-none', 'alert-success'], 2000)
+        }
+    }
+    else {
+        exibirAlerta('.alert-modal-login', 'Preencha todos os campos', ['show',
+            'alert-danger'], ['d-none', 'alert-success'], 2000)
+    }
+}
+
+{/* exemplo depoimento :
+     <div
+    class="col-lg-3 col-md-4 p-4 border border-secondary-subtle rounded-4 align-items-center bg-white">
+    <h5>[...]Já falei pra Agatha que se tiver o curso de novo vou me alistar para ensinar os outros
+        KKKK[...]</h5>
+    <p class="text-start fw-semibold">Gabriel José, 16 anos</p>
+</div> */}
+function exibirDepoimentos(depoimentos) {
+    const corpoDepoimentos = document.getElementById("depoimentos");
+    for(let depoimento of depoimentos){
+        const divDepoimento = document.createElement("div");
+        divDepoimento.className = "col-lg-3 col-md-4 p-4 border border-secondary-subtle rounded-4 align-items-center bg-white";
+        const textoDepoimento = document.createElement("h5");
+        textoDepoimento.innerHTML = `[...] ${depoimento.texto_depoimento} [...]`;
+        const infoPessoa = document.createElement("p");
+        infoPessoa.className = "text-start fw-semibold";
+        infoPessoa.innerHTML = `${depoimento.info_pessoa}`;      
+        divDepoimento.append(textoDepoimento);
+        divDepoimento.append(infoPessoa);  
+        corpoDepoimentos.append(divDepoimento);
+    }
+}
+
+async function obterDepoimentos() {
+    const depoimentosEndPoint = '/depoimentos';
+    const URLcompleta = `${protocolo}${baseURL}${depoimentosEndPoint}`;
+    const depoimentos = (await axios.get(URLcompleta)).data;
+    exibirDepoimentos(depoimentos);
+}
